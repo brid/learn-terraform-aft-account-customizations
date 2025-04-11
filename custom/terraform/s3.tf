18df405826fc6c8bce3e99552d42aea7be77d264
Mark Bridgett <mark@omni.network>
2025-04-11 10:34:12 +0100

    Added a new s3 bucket to the custom customization

--- custom/terraform/s3.tf
+++ custom/terraform/s3.tf
@@ -4,3 +4,8 @@ resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-custom-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
+
+resource "aws_s3_bucket" "sandbox_bucket_2" {
+  bucket = "aft-custom-${data.aws_caller_identity.current.account_id}-new"
+  acl    = "private"
+}
